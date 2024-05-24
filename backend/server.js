37a455e74f9cd7a2c3f7e74b46e6216a89380338
theeshyguy <t.siopes00@yahoo.com>
2024-05-24 15:09:37 -0400

    changed * to / for testing

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname, "../frontend/index.html")));
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist'))
 })
 
